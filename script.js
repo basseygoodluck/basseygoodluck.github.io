151f29daa2220cf27783598b92ac6adbe7e12f64
GoodLuck <82160168+basseygoodluck@users.noreply.github.com>
2024-02-29 18:45:55 +0100

    changed code timer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Feb 29, 2024 00:00:00:000").getTime();
+var countDownDate = new Date("Mar 06, 2024 00:00:00:000").getTime();
     var x = setInterval(function() {
         var now = new Date().getTime();
         var distance = countDownDate - now;
